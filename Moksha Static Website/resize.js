6e270c523a5290247938b48cb05b0d8c5103bd2a
Shubham Kumaram <krm.shubham@gmail.com>
2016-02-11 00:40:35 +0530

    Added the new logos, minor fixes in resize function

--- Moksha Static Website/resize.js
+++ Moksha Static Website/resize.js
@@ -1,7 +1,20 @@
-if (document.documentElement.clientWidth>=1000)
-{
+var resizeFunc = function(){
 	var imgb = document.getElementById("bckimg");
 	var overlay = document.getElementById("overlay");
-	imgb.style.height = window.innerHeight+"px";
-	overlay.style.height = imgb.style.height;
+	if (document.documentElement.clientWidth>=1000)
+	{
+		imgb.style.height = window.innerHeight+"px";
+		overlay.style.height = imgb.style.height;
+	}
+	else
+	{
+		imgb.style.height = "100%";
+		overlay.style.height = "100%";
+	}
 }
+
+window.onload = resizeFunc;
+window.onresize = resizeFunc;
+
+window.onload();
+window.onresize();
