aded7227d92b80274b0bcf9708ba25eebee24dfc
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-02-22 17:31:26 +0530

    database schema added

--- Main/api/sql/moksha.sql
+++ Main/api/sql/moksha.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.11
+-- version 4.0.10.7
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 21, 2016 at 03:38 PM
--- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
+-- Host: localhost:3306
+-- Generation Time: Feb 22, 2016 at 12:00 PM
+-- Server version: 5.6.22-72.0-log
+-- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +14,13 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `moksha`
+-- Database: `csinseew_moksha`
 --
+CREATE DATABASE IF NOT EXISTS `csinseew_moksha` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `csinseew_moksha`;
 
 -- --------------------------------------------------------
 
@@ -28,13 +30,43 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
-  `event_id` int(11) NOT NULL,
-  `event_name` varchar(300) DEFAULT NULL,
-  `image` varchar(200) NOT NULL,
-  `descp` varchar(300) NOT NULL,
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_name` varchar(300) NOT NULL,
+  `event_category` int(11) NOT NULL,
+  `image` varchar(1000) DEFAULT NULL,
+  `descp` text,
   `date` varchar(300) DEFAULT NULL,
-  `team_size` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `team_size` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_categories`
+--
+
+DROP TABLE IF EXISTS `event_categories`;
+CREATE TABLE IF NOT EXISTS `event_categories` (
+  `event_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_category_name` varchar(500) NOT NULL,
+  PRIMARY KEY (`event_category_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_registration`
+--
+
+DROP TABLE IF EXISTS `event_registration`;
+CREATE TABLE IF NOT EXISTS `event_registration` (
+  `event_registration_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `timestamp` varchar(200) NOT NULL,
+  PRIMARY KEY (`event_registration_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -44,12 +76,13 @@ CREATE TABLE IF NOT EXISTS `events` (
 
 DROP TABLE IF EXISTS `team`;
 CREATE TABLE IF NOT EXISTS `team` (
-  `team_id` int(11) NOT NULL,
+  `team_id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` int(11) NOT NULL,
   `user_id` varchar(11) NOT NULL,
   `email_id` varchar(300) NOT NULL,
-  `team_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `team_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`team_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -73,56 +106,16 @@ CREATE TABLE IF NOT EXISTS `team-users` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(1000) NOT NULL,
   `email` varchar(1000) NOT NULL,
   `password` varchar(1000) NOT NULL,
   `phone` varchar(50) NOT NULL,
-  `college` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `college` varchar(1000) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `events`
---
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`event_id`);
-
---
--- Indexes for table `team`
---
-ALTER TABLE `team`
-  ADD PRIMARY KEY (`team_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `phone` (`phone`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `events`
---
-ALTER TABLE `events`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `team`
---
-ALTER TABLE `team`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
