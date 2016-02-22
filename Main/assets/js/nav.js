693b95e5a74beb4805574ec8d0e6248c40ab35a5
Shubham Kumaram <krm.shubham@gmail.com>
2016-02-22 18:09:58 +0530

    added accomodations page to index.html

--- Main/assets/js/nav.js
+++ Main/assets/js/nav.js
@@ -1,38 +1,62 @@
 var nav = document.getElementById('panel'),
 	competitions = document.getElementById('competitions-page'),
 	event_category = document.getElementById('event-category-page'),
-	event_page = document.getElementById('event-page');
+	event_page = document.getElementById('event-page'),
+	accomodation_page = document.getElementById('accomodation-page');
 
 
 for (var i = nav.getElementsByTagName('li').length - 1; i >= 0; i--) {
 	nav.getElementsByTagName('li')[i].onclick = function(){
 		switch(this.innerHTML){
 			case "Home":
-				competitions.style.left= "10%";
-				event_category.style.left = "100%";
-				event_page.style.left = "100%";
-				
+				accomodation_page.style.visibility = "hidden";
 				competitions.style.visibility = "visible";
 				event_category.style.visibility = "hidden";
 				event_page.style.visibility = "hidden";
-				break;
-			case "Events":
-				competitions.style.left="-100%";
-				event_category.style.left = "0%";
-				event_page.style.left = "100%";
 				
+				competitions.style.left= "10%";
+				event_category.style.left = "100%";
+				event_page.style.left = "100%";
+				accomodation_page.style.left = "100%";
+				
+				break;
+				
+			case "Events":
+				accomodation_page.style.visibility = "hidden";
 				event_category.style.visibility = "visible";
 				competitions.style.visibility = "hidden";
 				event_page.style.visibility = "hidden";
+
+				competitions.style.left="-100%";
+				event_category.style.left = "0%";
+				event_page.style.left = "100%";
+				accomodation_page.style.left = "100%";
+				
 				break;
 			case "Competitions":
+				accomodation_page.style.visibility = "hidden";
+				event_page.style.visibility = "visible";
+				event_category.style.visibility = "hidden";
+				competitions.style.visibility = "hidden";
+				
 				competitions.style.left = "-100%";
 				event_category.style.left = "-100%";
 				event_page.style.left = "0%";
+				accomodation_page.style.left = "100%";
 				
-				event_page.style.visibility = "visible"
+				break;
+				
+			case "Accomodation":
+				accomodation_page.style.visibility = "visible";
+				event_page.style.visibility = "hidden";
 				event_category.style.visibility = "hidden";
 				competitions.style.visibility = "hidden";
+				
+				competitions.style.left = "-100%";
+				event_category.style.left = "-100%";
+				event_page.style.left = "-100%";
+				accomodation_page.style.left = "10%";
+				
 				break;
 		}
 	};
