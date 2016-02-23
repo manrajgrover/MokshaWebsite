46c65ac577cb10fbfe139b9b316699fefd0659dc
Mayank Badola <badola21295@gmail.com>
2016-02-23 13:58:21 +0530

    updated MokshaWebsite

--- Main/assets/js/site.js
+++ Main/assets/js/site.js
@@ -1,5 +1,6 @@
 $(function(){
   var comp;
+  var comp_itr;
   $.ajax({
     url:'../../../api/events/categories.php',
     type:'get',
@@ -14,7 +15,6 @@ $(function(){
   $.ajax({
     url:'../../../api/events/event_list.php',
     type:'get',
-    cache:false,
     dataType:'json',
     success:function(r){
       console.log(r);
@@ -24,9 +24,33 @@ $(function(){
       $('#event-page>.content>.rules-prizes>.rules>.section-desc').empty().append(r[0].rules);
       $('#event-page>.content>.rules-prizes>.prizes>.section-desc').empty().append(r[0].prizes);
       $('#event-page>.content>.large-event-pic>img').attr('src',r[0].image_url);
+      comp_itr = 1;
     },
     error:function(e){
       console.log(e);
     }
   });
+
+  $('#event-page>.right-clickable-box').click(function(){
+    if(comp_itr==comp.length)
+    comp_itr=0;
+    $('#event-page>.content>.event>header>h3').text(comp[comp_itr].name);
+    $('#event-page>.content>.event>.section-desc').empty().append('<p>'+comp[comp_itr].description+'</p>');
+    $('#event-page>.content>.rules-prizes>.rules>.section-desc').empty().append(comp[comp_itr].rules);
+    $('#event-page>.content>.rules-prizes>.prizes>.section-desc').empty().append(comp[comp_itr].prizes);
+    $('#event-page>.content>.large-event-pic>img').attr('src',comp[comp_itr].image_url);
+    ++comp_itr
+  });
+
+  $('#event-page>.left-clickable-box').click(function(){
+    if(comp_itr==-1)
+    comp_itr=comp.length-1;
+    $('#event-page>.content>.event>header>h3').text(comp[comp_itr].name);
+    $('#event-page>.content>.event>.section-desc').empty().append('<p>'+comp[comp_itr].description+'</p>');
+    $('#event-page>.content>.rules-prizes>.rules>.section-desc').empty().append(comp[comp_itr].rules);
+    $('#event-page>.content>.rules-prizes>.prizes>.section-desc').empty().append(comp[comp_itr].prizes);
+    $('#event-page>.content>.large-event-pic>img').attr('src',comp[comp_itr].image_url);
+    --comp_itr
+  });
+
 });
