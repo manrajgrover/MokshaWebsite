a772502b91da655e0e2bf4021856bc95c0e47c86
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-02-23 14:47:36 +0530

    event registration check added

--- Main/api/register/new.php
+++ Main/api/register/new.php
@@ -3,7 +3,7 @@
  * @Author: Prabhakar Gupta
  * @Date:   2016-02-21 15:22:42
  * @Last Modified by:   Prabhakar Gupta
- * @Last Modified time: 2016-02-21 15:36:51
+ * @Last Modified time: 2016-02-23 14:47:01
  */
 session_start();
 $_SESSION['user_id'] = 1;
@@ -22,16 +22,26 @@ $event_id 	= (int)$_GET['event_id'];
 $current_timestamp = time();
 
 $success 	= false;
+$already 	= false;
 $user 		= null;
 
 if($user_id > 0){
-	$query = "INSERT INTO `event_registration` (`event_id`,`user_id`,`timestamp`) VALUES ('$event_id', '$user_id','$current_timestamp')";
-	$success = (bool)mysqli_query($connection, $query);
+	$query_check = "SELECT `timestamp` FROM `event_registration` WHERE `event_id`='$event_id' AND `user_id`='$user_id' LIMIT 1";
+	$query_check_row = mysqli_fetch_assoc(mysqli_query($connection, $query_check));
+
+	if(isset($query_check_row)){
+		$already = true;
+	} else {
+		$query = "INSERT INTO `event_registration` (`event_id`,`user_id`,`timestamp`) VALUES ('$event_id', '$user_id','$current_timestamp')";
+		$success = (bool)mysqli_query($connection, $query);
+	}
+
 }
 
 
 $final_response = array(
 	'success' 	=> (bool)$success,
+	'already'	=> (bool)$already,
 );
 session_destroy();
 echo json_encode($final_response);
