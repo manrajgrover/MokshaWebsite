f82fc7a8e577b82e8f519ae562420d8e6c941ad3
Mayank Badola <badola21295@gmail.com>
2016-03-09 12:04:50 +0530

    local commit

--- Main/api/account/signup.php
+++ Main/api/account/signup.php
@@ -23,12 +23,13 @@ if($email_valid && !empty($name) && !empty($password) && !empty($phone) && !empt
 		$success = true;
 		$moksha_id = (int)mysqli_insert_id($connection);
 		$mok_id = "MOK-" . $moksha_id . "";
+		$name = ucwords($name);
 		$sql="UPDATE `users` SET `mok_id`='$mok_id'WHERE `phone`='$phone'";
 		mysqli_query($connection,$sql);
 		$to = "$email";
         $subject = "Moksha 2016 Registration";
-        $message ="<p>Dear $name,
-        Your have <b>successfully registered</b> for Moksha 2016.Your <b>Moksha ID</b> is <b>$mok_id</b>.You'll need to show the below QR code to the security personnel at Moksha.Please keep it with you at all times.</p>
+        $message ="<p>Dear $name,<br><br>
+        You have successfully registered for Moksha 2016. Your Moksha ID is <b>$mok_id</b>.<br><br>You'll need to show the below QR code to the security personnel at Moksha. Please keep it with you at all times.</p>
 				<br><img src='https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=$mok_id'/>";
         $from = "publicrelations.moksha@gmail.com";
         $headers = "From: $from"."\nMIME-Version: 1.0\nContent-Type: text/html; charset=utf-8\n";
